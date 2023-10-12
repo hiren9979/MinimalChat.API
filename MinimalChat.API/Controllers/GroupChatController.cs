dbbb5b7afb126fde33e8b2e87be4f4bfdb99355e
hiren979 <hirenchavda141@gmail.com>
2023-10-12 11:09:14 +0530

    Implement API of add members to group & validation

--- MinimalChat.API/Controllers/GroupChatController.cs
+++ MinimalChat.API/Controllers/GroupChatController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using MinimalChat.Data.Services;
 using MinimalChat.Domain.DTO;
 using MinimalChat.Domain.Model;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace MinimalChat.API.Controllers
 {
@@ -17,11 +19,40 @@ namespace MinimalChat.API.Controllers
             _groupChatService = groupChatService;
         }
 
-        [HttpPost]
+        [HttpPost("CreateGroup")]
         public IActionResult CreateGroupChat([FromBody] GroupChatDTO model)
         {
             var createdGroupChat = _groupChatService.CreateGroupChat(model);
             return Ok(createdGroupChat);
         }
+
+        [HttpPost("{groupId}/add-members")]
+        public async Task<IActionResult> AddGroupMembers(string groupId, [FromBody] List<string> memberIds)
+        {
+            var updatedGroupChat = await _groupChatService.AddGroupMembers(groupId, memberIds);
+
+            if (updatedGroupChat == null)
+            {
+                Console.WriteLine("User is already exist in this group");
+                return BadRequest("User is already a member of this group.");
+
+            }
+            else
+            {
+                Console.WriteLine("Successfully Added !!!");
+            }
+
+            // Use JsonSerializerOptions with ReferenceHandler.Preserve to handle object cycles
+            var options = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            // Serialize the group chat with the Preserve reference handling
+            var serializedGroupChat = JsonSerializer.Serialize(updatedGroupChat, options);
+
+            return Ok(serializedGroupChat);
+        }
+
     }
 }
