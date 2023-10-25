642fc20b328ffad1aa467fde59422d10fbbd60a1
hiren979 <hirenchavda141@gmail.com>
2023-10-25 17:22:58 +0530

    Implement database schema of tagUserInGroup and create model

---
+++ MinimalChat.Domain/Model/TagUserMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalChat.Domain.Model
+{
+    public class TagUserMessage
+    {
+        public int Id { get; set; }
+
+        public string SenderId { get; set; }
+
+        public string ReceiverId { get; set; }
+
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public string GroupName { get; set; }
+    }
+}
